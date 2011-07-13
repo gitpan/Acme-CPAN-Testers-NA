a35660627a7d813ea1068afccea55cf41351c1b4
Chris Williams <chris@bingosnet.co.uk>
2011-07-13 12:52:00 +0000

    Import of BINGOS/Acme-CPAN-Testers-NA-0.02 from CPAN.
    gitpan-cpan-distribution: Acme-CPAN-Testers-NA
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         BINGOS/Acme-CPAN-Testers-NA-0.02.tar.gz
    gitpan-cpan-author:       BINGOS
    gitpan-cpan-maturity:     released

---
+++ lib/Acme/CPAN/Testers/NA.pm
@@ -0,0 +1,23 @@
+package Acme::CPAN::Testers::NA;
+
+use strict;
+use warnings;
+use vars qw[$VERSION];
+
+$VERSION = '0.02';
+
+q[semper propositum];
+
+=pod
+
+=head1 NAME
+
+Acme::CPAN::Testers::NA - generate an NA test report
+
+=head1 DESCRIPTION
+
+Acme::CPAN::Testers::NA generates an C<NA> test report when run by a CPAN Tester.
+
+That's pretty much it, really.
+
+=cut
